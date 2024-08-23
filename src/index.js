8e8b7a2ccc76035df7ef53c2493549b130389e8d
Petri Laine <petri.laine@almamedia.fi>
2024-08-23 16:38:56 +0300

    Add chuck

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-const popup = () => {
-  window.alert("test");
+const getRandomChuckNorrisWisdom = async () => {
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const json = await response.json();
+    const chuckWisdom = document.getElementById("chuck-wisdom");
+    const speechBubble = document.getElementById("speech-bubble");
+    const mainButton = document.getElementById("main-button");
+    const mainHeader = document.getElementById("main-header");
+    chuckWisdom.innerText = json.value;
+    speechBubble.style.display = "inline-block";
+    mainButton.style.visibility = "hidden";
+    mainHeader.style.visibility = "hidden";
+  } catch (e) {
+    console.error(e);
+    element.innerText = "Chuck is on vacation, try again later!";
+  }
 };
