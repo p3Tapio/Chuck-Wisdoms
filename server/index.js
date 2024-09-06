99335802b5c1d2fcd5202e0566592cdf9fc7decb
Petri Laine <petri.laine@almamedia.fi>
2024-09-06 16:16:08 +0300

    Add browser sync and reset button

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
+import browserSync from "browser-sync";
 import express from "express";
 
 const isTest = process.env.ENV === "test";
 const path = isTest ? "dist" : "src";
+const PORT = 5000;
 
 export const startServer = () => {
   const app = express();
   app.use(express.static(path));
-  const PORT = 5000;
   return app.listen(PORT, () => console.log(`Server listening port ${PORT}`));
 };
 
 if (!isTest) {
   startServer();
+
+  const sync = browserSync.create();
+  sync.init({
+    proxy: `localhost:${PORT}`,
+    files: [`${path}/**/*.*`],
+    port: PORT + 1,
+  });
 }
