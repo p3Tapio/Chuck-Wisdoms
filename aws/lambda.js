62acafc11fa30fce36224a2a608a541382ee593b
Petri Laine <petri.laine@almamedia.fi>
2024-09-16 17:26:32 +0300

    Try to fix js/fonts

--- aws/lambda.js
+++ aws/lambda.js
@@ -8,7 +8,7 @@ export const handler = async (event, context) => {
     {key: "Strict-Transport-Security", value: "max-age=63072000; includeSubdomains; preload"}
   ];
   headers["content-security-policy"] = [
-    {key: "Content-Security-Policy", value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"}
+    {key: "Content-Security-Policy", value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'; font-src 'self'"}
   ];
   headers["x-content-type-options"] = [
     {key: "X-Content-Type-Options", value: "nosniff"}
