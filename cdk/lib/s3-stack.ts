54db8b1faa27089a3cbbfb5dd95605c6d50a0cc4
Petri Laine <petri.laine@almamedia.fi>
2024-11-29 17:47:20 +0200

    Edit cdk build, remove cdk test files for now

--- cdk/lib/s3-stack.ts
+++ cdk/lib/s3-stack.ts
@@ -8,33 +8,11 @@ export class S3Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const ACCOUNT_ID = cdk.Stack.of(this).account;
-
     this.bucket = new s3.Bucket(this, "StaticWebsiteBucket", {
       bucketName: "chuckie-wisdoms",
       publicReadAccess: false,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.RETAIN,
     });
-    /*
-    const bucketPolicy = new s3.BucketPolicy(this, "BucketPolicy", {
-      bucket: this.bucket,
-    });
-
-    bucketPolicy.document.addStatements(
-      new iam.PolicyStatement({
-        sid: "2",
-        effect: iam.Effect.ALLOW,
-        principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
-        actions: ["s3:GetObject"],
-        resources: [this.bucket.arnForObjects("*")],
-        conditions: {
-          StringEquals: {
-            "AWS:SourceArn": `arn:aws:cloudfront::${ACCOUNT_ID}:distribution/YOUR_DISTRIBUTION_ID`,
-          },
-        },
-      })
-    );
-    */
   }
 }
